45a057ce644b5f2535843e36131a107473eb65ec
Samuel Adebayo-Adesina <sam.adebayoadesina@gmail.com>
2021-12-10 15:14:10 +0100

    Added Views

--- hostel/allocation/views.py
+++ hostel/allocation/views.py
@@ -1,4 +1,38 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HostelList(APIView):
+
+    def get(self, request, format=None):
+        pass
+
+    def post():
+        pass
+
+    def put():
+        pass
+
+    def delete():
+        pass
+
+
+
+class HostelList(APIView):
+
+    def get(self, request, format=None):
+        pass
+
+    def post():
+        pass
+
+    def put():
+        pass
+
+    def delete():
+        pass
+
+
+def index(request):
+    pass
