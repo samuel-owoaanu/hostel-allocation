13ddc2984ee4d20f256f95afdda1947252e80e8a
milinches <ibrogenius5165@gmail.com>
2021-12-11 09:14:14 +0100

    Fixed Assertion Error

--- hostel/allocation/views.py
+++ hostel/allocation/views.py
@@ -1,14 +1,28 @@
+from os import stat
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
+from rest_framework. response import Response
+from rest_framework import serializers, status
+from django.http import Http404
 
 # Create your views here.
-class HostelList(APIView):
+class HostelListView(APIView):
+
+    def get_hostel(self):
+        try:
+            return Room.objects.all()
+        except Room.DoesNotExist:
+            raise Http404
 
     def get(self, request, format=None):
-        pass
+        rooms = self.get_hostel()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post():
+    def post(): 
         pass
 
     def put():
