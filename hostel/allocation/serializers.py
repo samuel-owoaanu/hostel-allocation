e3a77a35d659236365c6042ce8fbd22b8b6ee0cc
milinches <ibrogenius5165@gmail.com>
2021-12-11 13:37:18 +0100

    Created a new models and serializer

--- hostel/allocation/serializers.py
+++ hostel/allocation/serializers.py
@@ -5,4 +5,10 @@ from .models import *
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = ('room_number',)
+
+class HostelSerializer(serializers.ModelSerializer):
+    rooms = serializers.StringRelatedField(many=True, read_only=True)
+    class Meta:
+        model = Hostel
+        fields = ('id', 'hostel_name', 'hostel_type', 'hostel_code', 'hall_master', 'chief_porter', 'rooms')
