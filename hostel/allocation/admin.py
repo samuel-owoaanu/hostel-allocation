97e677b109e25db11396675e55f0519c104afcc3
milinches <ibrogenius5165@gmail.com>
2021-12-28 10:41:32 +0100

    Revamp

--- hostel/allocation/admin.py
+++ hostel/allocation/admin.py
@@ -4,6 +4,3 @@ from .models import *
 # admin - username
 # password - admin12345
 # Register your models here.
-
-admin.site.register(Room)
-admin.site.register(Hostel)
--- hostel/allocation/urls.py
+++ hostel/allocation/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from .views import *
 
 urlpatterns = [
-    path('api/v1', HostelListView.as_view(), name='hostels'),
-    # path('', index)
 ]
